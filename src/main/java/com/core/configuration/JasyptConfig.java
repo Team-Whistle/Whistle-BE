bc083674f98b9013ff25777c7a9292a80614e193
uihyeon1229 <101084941+uihyeon1229@users.noreply.github.com>
2023-12-12 01:26:54 +0900

    [EHJEONG][JASYPT 암호화]

--- src/main/java/com/core/configuration/JasyptConfig.java
+++ src/main/java/com/core/configuration/JasyptConfig.java
@@ -8,22 +8,24 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-class JasyptConfig {
+public class JasyptConfig {
 
     @Value("${jasypt.encryptor.password}")
     private String encryptKey;
 
     @Bean("jasyptStringEncryptor")
     public StringEncryptor stringEncryptor() {
-
         PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
         SimpleStringPBEConfig config = new SimpleStringPBEConfig();
-
-        config.setPassword(encryptKey); // encrypt key
-
-        config.setAlgorithm("PBEWITHMD5ANDDES");
+        System.out.println("encryptKey : " + encryptKey);
+        config.setPassword(encryptKey);
+//        config.setPassword(System.getenv("JASYPT_PASSWORD")); // 환경 변수
+        config.setAlgorithm("PBEWithMD5AndDES");
+        config.setKeyObtentionIterations("1000");
         config.setPoolSize("1");
+        config.setProviderName("SunJCE");
         config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setIvGeneratorClassName("org.jasypt.iv.RandomIvGenerator");
         config.setStringOutputType("base64");
         encryptor.setConfig(config);
         return encryptor;
