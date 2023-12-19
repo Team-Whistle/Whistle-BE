8fb43bcb26e478cd3428fcdd7e05cac76810ff1f
uihyeon1229 <101084941+uihyeon1229@users.noreply.github.com>
2023-12-20 02:16:21 +0900

    [EHJEONG][cors 수정]

--- src/main/java/com/core/configuration/SecurityConfiguration.java
+++ src/main/java/com/core/configuration/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import com.core.security.jwt.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -13,6 +14,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 /**
  * Created on July, 2023
@@ -50,6 +54,21 @@ public class SecurityConfiguration {
 
 		//@formatter:on
 	}
+
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource(){
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.addAllowedOrigin("http://localhost:3000");
+		configuration.addAllowedMethod("*");
+		configuration.addAllowedHeader("*");
+		configuration.setAllowCredentials(true);
+		configuration.validateAllowCredentials();
+		configuration.addExposedHeader(HttpHeaders.AUTHORIZATION);
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
+
 
 
 }
